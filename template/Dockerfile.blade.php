e29e51eeb2741736f81dc3ce473e193872663ff1
Daniel Polito <danielbpolito@gmail.com>
2020-11-17 19:07:08 -0300

    Force Composer to 1

--- template/Dockerfile.blade.php
+++ template/Dockerfile.blade.php
@@ -65,7 +65,7 @@ RUN adduser -D -u 1337 kool \
         zip \
     && cp "/usr/local/etc/php/php.ini-{{ $prod ? 'production' : 'development' }}" "/usr/local/etc/php/php.ini" \
     # composer
-    && curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer \
+    && curl -sS https://getcomposer.org/installer | php -- --1 --install-dir=/usr/local/bin --filename=composer \
     && su-exec kool composer global require hirak/prestissimo \
     # cleanup
     && apk del .build-deps \
