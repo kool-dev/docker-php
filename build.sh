c7a762d6b52b9bb6845058c9c99d2522f06eb186
fabriciojs <fajosesouza@gmail.com>
2020-11-26 14:14:20 -0300

    remove wkhtmltopdf version in favour of kool-dev/pdf

--- build.sh
+++ build.sh
@@ -2,57 +2,25 @@
 
 docker build --pull -t kooldev/php:7.1 7.1
 docker build --pull -t kooldev/php:7.1-prod 7.1-prod
-docker build -t kooldev/php:7.1-wkhtmltopdf 7.1-wkhtmltopdf
-docker build -t kooldev/php:7.1-wkhtmltopdf-prod 7.1-wkhtmltopdf-prod
 docker build -t kooldev/php:7.1-nginx 7.1-nginx
 docker build -t kooldev/php:7.1-nginx-prod 7.1-nginx-prod
-docker build -t kooldev/php:7.1-nginx-wkhtmltopdf 7.1-nginx-wkhtmltopdf
-docker build -t kooldev/php:7.1-nginx-wkhtmltopdf-prod 7.1-nginx-wkhtmltopdf-prod
-docker build -t kooldev/php:7.1-full 7.1-full
-docker build -t kooldev/php:7.1-full-prod 7.1-full-prod
-docker build -t kooldev/php:7.1-full-wkhtmltopdf 7.1-full-wkhtmltopdf
-docker build -t kooldev/php:7.1-full-wkhtmltopdf-prod 7.1-full-wkhtmltopdf-prod
 
 docker build --pull -t kooldev/php:7.2 7.2
 docker build --pull -t kooldev/php:7.2-prod 7.2-prod
-docker build -t kooldev/php:7.2-wkhtmltopdf 7.2-wkhtmltopdf
-docker build -t kooldev/php:7.2-wkhtmltopdf-prod 7.2-wkhtmltopdf-prod
 docker build -t kooldev/php:7.2-nginx 7.2-nginx
 docker build -t kooldev/php:7.2-nginx-prod 7.2-nginx-prod
-docker build -t kooldev/php:7.2-nginx-wkhtmltopdf 7.2-nginx-wkhtmltopdf
-docker build -t kooldev/php:7.2-nginx-wkhtmltopdf-prod 7.2-nginx-wkhtmltopdf-prod
-docker build -t kooldev/php:7.2-full 7.2-full
-docker build -t kooldev/php:7.2-full-prod 7.2-full-prod
-docker build -t kooldev/php:7.2-full-wkhtmltopdf 7.2-full-wkhtmltopdf
-docker build -t kooldev/php:7.2-full-wkhtmltopdf-prod 7.2-full-wkhtmltopdf-prod
 
 docker build --pull -t kooldev/php:7.3 7.3
 docker build --pull -t kooldev/php:7.3-prod 7.3-prod
-docker build -t kooldev/php:7.3-wkhtmltopdf 7.3-wkhtmltopdf
-docker build -t kooldev/php:7.3-wkhtmltopdf-prod 7.3-wkhtmltopdf-prod
 docker build -t kooldev/php:7.3-nginx 7.3-nginx
 docker build -t kooldev/php:7.3-nginx-prod 7.3-nginx-prod
-docker build -t kooldev/php:7.3-nginx-wkhtmltopdf 7.3-nginx-wkhtmltopdf
-docker build -t kooldev/php:7.3-nginx-wkhtmltopdf-prod 7.3-nginx-wkhtmltopdf-prod
-docker build -t kooldev/php:7.3-full 7.3-full
-docker build -t kooldev/php:7.3-full-prod 7.3-full-prod
-docker build -t kooldev/php:7.3-full-wkhtmltopdf 7.3-full-wkhtmltopdf
-docker build -t kooldev/php:7.3-full-wkhtmltopdf-prod 7.3-full-wkhtmltopdf-prod
 
 docker build --pull -t kooldev/php:7.4 7.4
 docker build --pull -t kooldev/php:7.4-prod 7.4-prod
-docker build -t kooldev/php:7.4-wkhtmltopdf 7.4-wkhtmltopdf
-docker build -t kooldev/php:7.4-wkhtmltopdf-prod 7.4-wkhtmltopdf-prod
 docker build -t kooldev/php:7.4-nginx 7.4-nginx
 docker build -t kooldev/php:7.4-nginx-prod 7.4-nginx-prod
-docker build -t kooldev/php:7.4-nginx-wkhtmltopdf 7.4-nginx-wkhtmltopdf
-docker build -t kooldev/php:7.4-nginx-wkhtmltopdf-prod 7.4-nginx-wkhtmltopdf-prod
 
 docker build --pull -t kooldev/php:8.0 8.0
 docker build --pull -t kooldev/php:8.0-prod 8.0-prod
-docker build -t kooldev/php:8.0-wkhtmltopdf 8.0-wkhtmltopdf
-docker build -t kooldev/php:8.0-wkhtmltopdf-prod 8.0-wkhtmltopdf-prod
 docker build -t kooldev/php:8.0-nginx 8.0-nginx
 docker build -t kooldev/php:8.0-nginx-prod 8.0-nginx-prod
-docker build -t kooldev/php:8.0-nginx-wkhtmltopdf 8.0-nginx-wkhtmltopdf
-docker build -t kooldev/php:8.0-nginx-wkhtmltopdf-prod 8.0-nginx-wkhtmltopdf-prod
